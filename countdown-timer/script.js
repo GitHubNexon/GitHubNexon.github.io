a4f02f372b1543feb6e9a486505442c4c4b3fac6
GitHubNexon <134722556+GitHubNexon@users.noreply.github.com>
2023-08-31 22:53:29 +0800

    Add files via upload

---
+++ countdown-timer/script.js
@@ -0,0 +1,26 @@
+let hamMenuIcon = document.getElementById("ham-menu");
+let navBar = document.getElementById("nav-bar");
+let navLinks = navBar.querySelectorAll("li");
+hamMenuIcon.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+  hamMenuIcon.classList.toggle("fa-times");
+});
+navLinks.forEach((navLinks) => {
+  navLinks.addEventListener("click", () => {
+    navBar.classList.remove("active");
+    hamMenuIcon.classList.toggle("fa-times");
+  });
+});
+
+
+// for nav bar
+
+
+
+
+// for audio
+var audio = document.getElementById("autoplayAudio");
+
+audio.addEventListener("canplaythrough", function() {
+  audio.play();
+});
